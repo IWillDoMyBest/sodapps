77eda28e9482087db5cf437c57d0224ef2e65065
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2019-02-05 12:37:25 +0100

    updated files

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ var app = express();
 var port = 3000;
 var server = http.createServer(app);
 
+var valid_key = "bondhon";
+
 app.set("view engine", "hbs");
 app.engine("html", require("hbs").__express);
 
 app.use(express.static(__dirname + "/static"));
+
+Date.prototype.getWeek = function() {
+    var dt = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - dt) / 86400000) + dt.getDay()+1)/7);
+};
 
 var index_route = require("./routes/index");
 var food_route = require("./routes/food");
@@ -21,6 +28,19 @@ app.use("/", index_route);
 app.use("/food", food_route);
 app.use("/schedule", schedule_route);
 app.use("/project", project_route);
+
+app.get("/api/:key", (req, res) => {
+	var key = req.params.key;
+	if (key != valid_key){
+		return res.send("Invalid key");
+	}
+	fs.readFile("food.json", (err, data) => {
+		if (err){
+			return res.send(err);
+		}
+		return res.json(JSON.parse(data));
+	});
+});
 
 app.get("*", (req, res) => {
 	return res.send("<title>Error</title>Error 404, page not found. <a href='/'>Go back.</a>");
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+var fs = require("fs");
 
-var food = fs.readFile("food.json", (err, data) => {
 
-	if (err){
-		return (err);
-	}
-	
-	return (data);
-
-});
 
 router.route("/")
 	
 	.get((req,res) => {
+
+		var jsonFood = fs.readFileSync("food.json");
+		var food = JSON.parse(jsonFood);
+
+		var date = new Date();
+		var week = date.getWeek();
+		var day = date.getDay();
+		var todaysFood = food[week][day];
 
 		return res.render("index.hbs", {
 			user: {
 				"name": "Pierre",
 				"class": "16_TEK_CS"
 			},
-			food
+			todaysFood
 		});
 
 	});
